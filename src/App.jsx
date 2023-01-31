64e93a7cbfe4bfa92b4dd17699c7c299413d9e4c
Josimar Junior <juniorcvlct17@gmail.com>
2023-01-31 01:57:01 -0400

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// import Contact from "./components/Contact";
+import Header from "./components/Header";
+import NavIcon from "./components/NavIcon";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Recipe from "./components/Recipe";
+// import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="font-poppins bg-back">
+      <Header />
+      {/* <NavIcon /> */}
+      {/* <Home />
+      <About />
+      <Recipe />
+      <Contact />
+      <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
